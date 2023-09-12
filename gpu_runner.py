b67f5aec77bfe7e5e7cc13eb3073fca89b72dfb6
rober <rober@DESKTOP-VGEREEB.home>
2023-09-12 00:22:38 -0400

    added gpu support

---
+++ gpu_runner.py
@@ -0,0 +1,78 @@
+from numba import cuda
+
+        
+@cuda.jit
+def run_strat(datapoints, data):
+    thread_position = cuda.grid(1)
+    #current_data = data[thread_position]
+    
+    #print(current_data)
+    buy_on_diff_percent=datapoints[thread_position][0]
+    sell_on_diff_percent=datapoints[thread_position][1]
+    range_b=datapoints[thread_position][2]
+    range_s=datapoints[thread_position][3]
+    
+    buy_on_diff_percent_min=buy_on_diff_percent-(range_b/2)
+    buy_on_diff_percent_max=buy_on_diff_percent+(range_b/2)
+    sell_on_diff_percent_min=sell_on_diff_percent-(range_s/2)
+    sell_on_diff_percent_max=sell_on_diff_percent+(range_s/2)
+    start_money = 100000
+    avail_money = start_money
+    current_stocks = 0
+    profit_percent = None
+    transactions = 0
+    last_buy_time = 0
+    total_time_invested = 0
+    delay_milis=5000
+    
+    
+    
+    previous_price = None
+    set_buy = None
+    set_sell = None
+    transaction_time = None
+    for item in data:
+        price=previous_price
+        time = item[0]
+        price = item[1]
+
+        if previous_price is None:
+            previous_price = price
+        price_diff = ((price/previous_price)-1)*100
+        if transaction_time is not None:
+            if time > transaction_time:
+                if set_buy is not None:
+                    buying_amount = avail_money//price
+                    if buying_amount:
+                        transactions = transactions + 1
+                        last_buy_time = time
+                        current_stocks = current_stocks + buying_amount
+                        avail_money = avail_money - price*buying_amount
+                    set_buy = None
+                else:
+                    if current_stocks != 0:
+                        transactions = transactions + 1
+                        total_time_invested = total_time_invested + time - last_buy_time
+                        avail_money = avail_money + price*current_stocks
+                        current_stocks = 0
+                    set_sell = None
+                transaction_time = None
+        
+        elif price_diff >= buy_on_diff_percent_min and price_diff <= buy_on_diff_percent_max and set_buy is None and set_sell is None and avail_money//price:
+            set_buy = price
+            transaction_time = time+delay_milis
+        elif price_diff >= sell_on_diff_percent_min and price_diff <= sell_on_diff_percent_max and set_buy is None and set_sell is None and current_stocks:
+            set_sell = price
+            transaction_time = time+delay_milis
+
+    if current_stocks:
+        transactions = transactions + 1
+        total_time_invested = total_time_invested + time - last_buy_time
+        avail_money = avail_money + price*current_stocks
+        current_stocks = 0
+
+    profit = avail_money-start_money
+    
+    datapoints[thread_position][4] = profit
+    datapoints[thread_position][5] = transactions
+    datapoints[thread_position][6] = total_time_invested
--- strats.py
+++ strats.py
@@ -1,10 +1,14 @@
 import time
 import multiprocessing as mp
-from runner import Runner
+from gpu_runner import run_strat
+from cpu_runner import Runner
 from data_fetcher import DataFetcher
 import tqdm
 import argparse
 from graph_generator import gen_csv, show_graph
+from numba import cuda
+import numpy as np
+import math
 
 def main():
     parser = argparse.ArgumentParser()
@@ -15,8 +19,9 @@ def main():
     parser.add_argument("--end", default=16, type=float, help="End of day time")
     parser.add_argument("--buy", default=10, type=int, help="Buy max percent to test")
     parser.add_argument("--sell", default=10, type=int, help="Sell max percent to test")
-    parser.add_argument("--brange", default=1, type=int, help="Buy max percent range to test")
-    parser.add_argument("--srange", default=1, type=int, help="Sell max percent range to test")
+    parser.add_argument("--brange", default=4, type=int, help="Buy max percent range to test")
+    parser.add_argument("--srange", default=4, type=int, help="Sell max percent range to test")
+    parser.add_argument("--cpu", action='store_true', help="Use CPU instead of gpu")
     args = parser.parse_args()
 
     data_cache = DataFetcher()
@@ -24,32 +29,54 @@ def main():
         data_cache.poll_yfinance(args.symbol, args.days)
     else:
         data_cache.poll_aws_data(args.symbol, args.start, args.end)
-    datapoints = get_datapoints(data_cache, args.buy, args.sell, args.brange, args.srange)
-    results, start_time = run_strats_multiprocess(datapoints)
+    
+    if args.cpu:
+        datapoints = get_starmap_datapoints(data_cache, args.buy, args.sell, args.brange, args.srange)
+    else:
+        datapoints = get_datapoints(args.buy, args.sell, args.brange, args.srange)
+
+    if args.cpu:
+        results, start_time = run_strats_multiprocess(datapoints)
+    else:
+        results, start_time = run_strats_gpu(datapoints, data_cache)
     analyse_results(results, args.symbol, start_time)
 
-def get_datapoints(data_cache, buy, sell, brange, srange):
+def get_starmap_datapoints(data_cache, buy, sell, brange, srange):
     
     datapoints = [(data_cache, b/10, -(s/10), br/10, sr/10) for b in range(buy*(-10), buy*10+1) for s in range(sell*(-10), sell*10+1) for br in range(0, brange*10+1) for sr in range(0, srange*10+1)]
     print(f"number of datapoints: {len(datapoints)}")
     return datapoints
 
-def analyse_results(results, symbol, start_time):
+def get_datapoints(buy, sell, brange, srange):
+    def get_index(buy, sell, brange, srange):
+        return (20*buy+1)*(20*sell+1)*(brange*10+1)*(srange*10+1)
+    array = np.empty(shape=(get_index(buy, sell, brange, srange), 7), dtype=float)
+    i = 0
+    for b in range(buy*(-10), buy*10+1):
+        for s in range(sell*(-10), sell*10+1):
+            for br in range(0, brange*10+1):
+                for sr in range(0, srange*10+1):
+                    
+                    array[i]=np.array([b/10, -(s/10), br/10, sr/10, 0, 0, 0])
+                    i = i + 1
+    print(f"number of datapoints: {len(array)}")
+    return array
+
+def analyse_results(results, symbol, start_time, cpu=False):
     compiled = {}
     highest_profit = -100000
-    highest_profit_b = 0
-    highest_profit_s = 0
-    transactions = 0
-    invested_time = 0  
-    rbm = 0
-    rsm = 0
+    highest_profit_b = highest_profit_b = transactions = invested_time = rbm = rsm = 0
     
     for result in results:
-        buy, sell, profit, transactionsnb, itime, rb, rs= result.split("_")
-        profit=float(profit)
-        val = compiled.setdefault(f"{buy}_{sell}", 0)
-        cumul = val + profit
-        compiled.update({f"{buy}_{sell}":cumul})
+        if cpu:
+            buy, sell, profit, transactionsnb, itime, rb, rs= result.split("_")
+            profit=float(profit)
+
+        else:
+            buy, sell, rb, rs, profit, transactionsnb, itime = result.ravel()
+
+        cumul = compiled.setdefault(f"{buy}_{sell}_{rb}_{rs}", 0) + profit
+        compiled.update({f"{buy}_{sell}_{rb}_{rs}":cumul})
 
         if profit > highest_profit:
             highest_profit = profit
@@ -69,17 +96,27 @@ def analyse_results(results, symbol, start_time):
         if compiled[k] > max_val:
             max_val = compiled[k]
             key_val = k
-    print(f"max {max_val} buy {key_val.split('_')[0]} sell {key_val.split('_')[1]}")
+    print(f"multiple day cumulative profit: max {max_val} for buy at {key_val.split('_')[0]} and sell at {key_val.split('_')[1]} with rangeB {key_val.split('_')[2]} and rangeS {key_val.split('_')[3]}")
     #gen_csv(compiled)
     #show_graph(compiled)
 
+def run_strats_gpu(datapoints, data_cache): 
+    start_time = time.time()
+    data = data_cache.data
+    threadsperblock = 1024
+    blockspergrid = math.ceil(datapoints.shape[0] / threadsperblock)
+
+    np_data = np.array(data)
+    run_strat[blockspergrid, threadsperblock](datapoints, np_data)
+    
+    return datapoints, start_time
+ 
 def run_strats_multiprocess(datapoints): 
     start_time = time.time()
     p = mp.Pool()
     results = p.starmap(run_single, tqdm.tqdm(datapoints, total=len(datapoints)))
     p.close()
     return results, start_time
-    
 
 
 def run_single(cache, buy_on_diff_percent, sell_on_diff_percent, range_b, range_s, symbol="TQQQ", day_range=2):
